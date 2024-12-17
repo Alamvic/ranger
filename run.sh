6e1e0599840c0da11a6b0fcb7bea9d5f79408092
Guille Polito <guillermopolito@gmail.com>
2024-12-17 10:27:40 +0100

    Making it more reusable

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir build
 cd build
 
-wget -O - get.pharo.org/120 | bash
+wget -O - get.pharo.org/120+vm | bash
 ./pharo Pharo.image eval --save "Metacello new baseline: 'Ranger'; repository: 'gitlocal://${__dir}'; load"
 nohup ./pharo Pharo.image eval "MutationsExperiment main" > log.txt 2>&1 &
 
